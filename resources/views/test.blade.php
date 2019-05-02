f650f3d9f6f10a399f3b43f23ea5b664bed6b035
Phong Ngan <phongngan30091998@gmail.com>
2019-05-02 11:23:30 +0700

    k co j

---
+++ resources/views/test.blade.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+		<base href="{{asset('')}}">
+	<title></title>
+	<style type="text/css">
+		.red{background-color: red; }
+		.blue{background-color: blue; }
+		.gray{background-color: gray; }
+		.green{background-color: green; }
+		.black{background-color: black; }
+	
+	</style>
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body >	
+		<div class="dropdown open">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Dropdown
+			</button>
+			<div class="dropdown-menu" id="dropdown1" aria-labelledby="dropdownMenu1">
+				<a class="dropdown-item" href="#">Action</a>
+				<a class="dropdown-item" href="#">Action</a>
+				<a class="dropdown-item" href="#">Action</a>
+			</div>
+		</div>
+
+</body>
+</html>
+https://xpsp2p1.playphim.info/drive/hls/dfe7f7ebfde6d4d2dbc8a775ca969d86/dfe7f7ebfde6d4d2dbc8a775ca969d86162.html?ch=dfe7f7ebfde6d4d2dbc8a775ca969d86&s=4315559&l=236128
