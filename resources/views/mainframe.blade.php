60f63449743962f4379c17e225125b80ad614db5
Phong Ngan <phongngan30091998@gmail.com>
2019-05-08 18:41:40 +0700

    nang cap trang information.blade, khôi phục lại mainframe, edit, delete bình luận đang còn lỗi

--- resources/views/mainframe.blade.php
+++ resources/views/mainframe.blade.php
@@ -12,46 +12,18 @@
 	<!-- Loading third party fonts -->
 	<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
 	<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-	<!-- Loading main css file -->
 	<link rel="stylesheet" href="/css/style.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
 	<link rel="stylesheet" href="/css/login.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-
-
-	<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
 	<link rel="stylesheet" href="/css/signup.css">
+	<script src="{{asset('js/jquery-1.11.1.min.js')}}"></script>
 
-	<!--[if lt IE 9]>
-	<head>
-		<base href="{{asset('')}}">
-		<meta charset="UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+	 
 
-		@yield('title')
-		
-		<!-- Loading third party fonts -->
-		<link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
-		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-		<!-- Loading main css file -->
-		<link rel="stylesheet" href="/css/style.css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-		
-		<link rel="stylesheet" href="/css/login.css"> 
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-		
-		<!--[if lt IE 9]>
-		<script src="js/ie-support/html5.js"></script>
-		<script src="js/ie-support/respond.js"></script>
-		<![endif]-->
 </head>
 
 <body>
@@ -62,7 +34,7 @@
 		<header class="site-header">
 			<div class="container">
 				<a id="branding">
-					<img src="images/logo.png" alt="" class="logo">
+					<img src="/images/logo.png" alt="" class="logo">
 					<div class="logo-copy">
 						<h1 class="site-title">Company Name</h1>
 						<small class="site-description">Tagline goes here</small>
@@ -70,27 +42,25 @@
 				</a> <!-- #branding -->
 
 				<div class="main-navigation">
-					<form action="#" class="search-form">
-						<input type="text" placeholder="Search...">
-						<button><i class="fa fa-search"></i></button>
-					</form>
+					
 					<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
 
 					<ul class="menu">
 						<li class="menu-item current-menu-item"><a href="/">Home</a></li>
 						<li class="menu-item"><a href="/review">Movie reviews</a></li>
-						  @if(Auth::User())  <li class="menu-item"><a href="#"></a><b>{{Auth::User()->name}}<b></li>
-						  	<li class="menu-item"><a href="/LogOut">Đăng Xuất</a></li>
-  
-  
-@else 
+					@if(Auth::User())  
+						<li class="menu-item"><a href="#"></a><b>{{Auth::User()->name}}<b></li>
+						<li class="menu-item"><a href="/LogOut">Đăng Xuất</a></li>
+					@else 
 						<li class="menu-item"><a href="/login">Login</a></li>
 						<li class="menu-item"><a href="/signup">Sign up</a></li>
-						@endif
-						<li class="menu-item"><input type="text" placeholder="Search..." name="Search" id="TextSearch"></li>
-							<button id="ButtonSearch"><i class="fa fa-search"></i></button>
+					@endif
+						
 					</ul> <!-- .menu -->
-
+					<form action="#" class="search-form">
+						<input type="text" placeholder="Search...">
+						<button id="ButtonSearch"><i class="fa fa-search"></i></button>
+					</form>
 
 				</div> <!-- .main-navigation -->
 
@@ -174,13 +144,7 @@
 	<script src="{{asset('js/jquery-1.11.1.min.js')}}"></script>
 	<script src="{{asset('js/plugins.js')}}"></script>
 	<script src="{{asset('js/app.js')}}"></script>
-	<script src="https://cdnjs.com/libraries/jquery.pjax"></script>
-
-
-		<script src="js/jquery-1.11.1.min.js"></script>
-		<script src="js/plugins.js"></script>
-		<script src="js/app.js"></script>
-		
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.pjax/2.0.1/jquery.pjax.js"></script>
 
 	</body>
 
